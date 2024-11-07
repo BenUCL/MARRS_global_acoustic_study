a6fc7ca018a273d47f8dc12a778e431a7e1fdd10
Ben Williams <ben.williams.20@ucl.ac.uk>
2024-11-07 17:43:20 +0000

    minor changes

--- write_out_detections.py
+++ write_out_detections.py
@@ -1,13 +1,12 @@
 """This script can be used to write out the audio clips that were detected by the model.
-It reads the inference csv, finds the raw audio sample and writes out each 5sec clip that 
-was predicted to contain the target sound. Set the logit cutoff accordingly to filter by 
-confidence. It will first write the predictions with the lowest logit score, so that 
-these can be reviewed first if not running the script until completion. """
-
-# Only write out files where logits are equal to above this score
-LOGIT_CUTOFF = 1.0 # On croak, 0.5 gave a few false positives, on 1.0 it looked good almost always
-TARGET_COUNTRY = "indonesia"
-TARGET_SOUND = "croak"
+1. It reads the inference csv, finds the raw audio sample and writes out each 5sec clip that 
+was predicted to contain the target sound. 
+2. The new filename will begin with the logit score, then the timestamp in seconds, and 
+finally the original filename. TODO: Set so that new folders for each country and sound are
+created.
+3. Set the logit cutoff accordingly to filter by confidence. It will first write the 
+predictions with the lowest logit score, so that  these can be reviewed first if not running 
+the script until completion. """
 
 # Imports
 from pathlib import Path
@@ -16,15 +15,29 @@ import librosa
 import os
 import soundfile as sf
 from tqdm import tqdm
+from typing import Optional
 
+# Only write out files where logits are equal to above this score
+TARGET_COUNTRY = 'indonesia'
+TARGET_SOUND = 'scrape_fullband'
+# Only samples with a logit score above the value set below will be written. Set to 0 to write all samples. 
+LOGIT_CUTOFF = 1.0 
+# If set to None, all files will be written, otherwise will stop after this number of files.
+MAX_COUNT: Optional[int] = 100
+
+# Validate MAX_COUNT type
+assert MAX_COUNT is None or isinstance(MAX_COUNT, int), 'MAX_COUNT must be None or an integer'
+
+
+# Set base dir for filepaths
 base_dir = os.getenv('BASE_DIR')
 if not base_dir:
-    raise ValueError("BASE_DIR environment variable is not set.")
+    raise ValueError('BASE_DIR environment variable is not set.')
 
 # Define file paths
 csv_path = os.path.join(base_dir, 'marrs_acoustics/data/output_dir_' + TARGET_COUNTRY 
                         + '/agile_outputs/' + TARGET_SOUND + '/' + TARGET_SOUND + '_inference.csv')
-audio_base_path = Path("/media/mars_5tb_drive/mars_global_acoustic_study/" + TARGET_COUNTRY + "_acoustics/")
+audio_base_path = Path('/media/mars_5tb_drive/mars_global_acoustic_study/' + TARGET_COUNTRY + '_acoustics/')
 save_dir = Path(os.path.join(base_dir, 'marrs_acoustics/data/pred_audio_samples/' + TARGET_COUNTRY + '/' + TARGET_SOUND))
 
 # Create the save directory if it doesn't exist
@@ -37,9 +50,13 @@ df = pd.read_csv(csv_path)
 df = df[df[' logit'] >= LOGIT_CUTOFF].sort_values(by=' logit')
 
 # Process each row with tqdm for progress tracking
-for _, row in tqdm(df.iterrows(), total=len(df), desc="Processing audio clips"):
+for i, (_, row) in enumerate(tqdm(df.iterrows(), total=len(df), desc='Processing audio clips')):
+    # Exit loop if MAX_COUNT is reached
+    if MAX_COUNT is not None and i >= MAX_COUNT:
+        print(f'Stopping early because MAX_COUNT ({MAX_COUNT}) was reached.')
+        break  
+    # Beware leading white space for some column headers in the csv.
     filename, timestamp_s, label, logit = row['filename'], row[' timestamp_s'], row[' label'], row[' logit']
-    
     full_audio_path = audio_base_path / filename
 
     # Load audio
